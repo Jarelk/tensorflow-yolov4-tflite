d84daab612d285612f10b555453733ba655fdb3f
Jaimy Kwa <jaimykwa@gmail.com>
2021-03-05 14:45:39 +0100

    Hopefully it'll work now

---
+++ run/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("../README.md", 'r') as f:
+    long_description = f.read()
+
+REQUIRED = [
+  'tensorflow-gpu==2.3.0rc0',
+  'opencv-python==4.5.1.48',
+  'lxml', 
+  'tqdm',
+  'absl-py',
+  'matplotlib',
+  'easydict',
+  'pillow',
+]
+
+setup(
+   name='tensorflow-yolov4-tflite',
+   version='0.0.1',
+   url='https://github.com/Jarelk/tensorflow-yolov4-tflite/',
+   python_requires='==3.8.0',
+   description='Literally just trying to add a setup.py for easy installing',
+   packages=find_packages(),
+   install_requires=REQUIRED,
+   license='MIT'
+)
--- setup.py
+++ setup.py
@@ -1,26 +1,12 @@
-from setuptools import setup, find_packages
-
-with open("README.md", 'r') as f:
-    long_description = f.read()
-
-REQUIRED = [
-  'tensorflow-gpu==2.3.0rc0',
-  'opencv-python==4.5.1.48',
-  'lxml', 
-  'tqdm',
-  'absl-py',
-  'matplotlib',
-  'easydict',
-  'pillow',
-]
+from distutils.core import setup
 
 setup(
-   name='tensorflow-yolov4-tflite',
-   version='0.0.1',
-   url='https://github.com/Jarelk/tensorflow-yolov4-tflite/',
-   python_requires='==3.8.0',
-   description='Literally just trying to add a setup.py for easy installing',
-   packages=find_packages(),
-   install_requires=REQUIRED,
-   license='MIT'
+    name='tensorflow-yolov4-tflite-setup',
+    version='0.0.2',
+    packages=['mAP', 'run', 'core', 'data', 'scripts'],
+    url='https://github.com/Jarelk/tensorflow-yolov4-tflite',
+    license='MIT',
+    author='Jaimy',
+    author_email='',
+    description='Just creating a setup for the original repo'
 )
